4b880156d192bf97f5f1090b59a4cbbe44ce88c4
tonytani37 <36727784+tonytani37@users.noreply.github.com>
2021-01-27 10:43:07 +0900

    Update st_covid19.py

--- st_covid19.py
+++ st_covid19.py
@@ -118,8 +118,8 @@ plt.grid(True)
 
 
 data_n = [row['name_ja'] for row in summary_json['prefectures']] #都道府県名
-data_l = pd.DataFrame([row['dailyConfirmedCount'] for row in summary_json['prefectures']]) #感染者数
-data_d = pd.DataFrame([row['dailyDeceasedCount'] for row in summary_json['prefectures']]) #死亡者数
+data_l = [row['dailyConfirmedCount'] for row in summary_json['prefectures']] #感染者数
+data_d = [row['dailyDeceasedCount'] for row in summary_json['prefectures']] #死亡者数
 
 import datetime as dt
 
@@ -137,15 +137,15 @@ df_date = pd.DataFrame(date_idx,columns=['日付'])
 df_date = df_date['日付']
 df_n = pd.DataFrame(data_n)
 
-df_l = pd.DataFrame(data_l.tail(days),columns=df_date.tail(days))
-df_l['name_ja'] = df_n.tail(days)
+df_l = pd.DataFrame(data_l[-days],columns=df_date.tail(days))
+df_l['name_ja'] = df_n.tail(days).copy()
 # df_l = df_l.set_index('name_ja')
 df_sum = df_l.sum(axis=1)
 df_l['sum'] = df_sum
 df_l = df_l.fillna(0)
 
-df_d = pd.DataFrame(data_d.tail(days),columns=df_date.tail(days))
-df_d['name_ja'] = df_n.tail(days)
+df_d = pd.DataFrame(data_d[-days],columns=df_date.tail(days))
+df_d['name_ja'] = df_n.tail(days).copy()
 # df_d = df_d.set_index('name_ja')
 df_sum = df_d.sum(axis=1)
 df_d['sum'] = df_sum
