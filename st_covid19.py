ccc74f51a6e840dcae55f671bf692a7d3bab9d3c
tonytani37 <36727784+tonytani37@users.noreply.github.com>
2021-02-01 11:53:52 +0900

    2021/2/1 11:53

--- st_covid19.py
+++ st_covid19.py
@@ -16,6 +16,9 @@ try:
 except requests.exceptions.RequestException as err:
     print(err)
 
+# json_open = open('file\summary.json', 'r')
+# summary_json = json.load(json_open)
+
 collist = [
         'date',
         "confirmed",
@@ -182,8 +185,6 @@ import datetime as dt
 s_day = '2020-01-08' #開始日
 # s_day = '2020-01-18' #開始日
 
-# JST = timezone(timedelta(hours=+9), 'JST')
-# date_time = dt.datetime.now(JST)
 date_time = pd.to_datetime(df_d.iloc[-1])
 today = date_time.strftime("%Y-%m-%d")
 date_index = pd.date_range(s_day , today, freq="d")
@@ -194,113 +195,183 @@ df_date = pd.DataFrame(date_idx,columns=['日付'])
 df_date = df_date['日付']
 df_n = pd.DataFrame(data_n)
 
-# 内容表記でスライスは何故かダメみたい
-data_ls = []
-for row in data_l:
-    data_ls.append(row[-days:])
+# # 内容表記でスライスは何故かダメみたい
+# data_ls = []
+# for row in data_l:
+#     data_ls.append(row[-days:])
 
-df_l = pd.DataFrame(data_ls,columns=df_date)
-df_l['name_ja'] = df_n.tail(days).copy()
-# df_l = df_l.set_index('name_ja')
-df_sum = df_l.sum(axis=1)
-df_l['sum'] = df_sum
-df_l = df_l.fillna(0)
+# df_l = pd.DataFrame(data_ls,columns=df_date)
+# df_l['name_ja'] = df_n.tail(days).copy()
+# # df_l = df_l.set_index('name_ja')
+# df_sum = df_l.sum(axis=1)
+# df_l['sum'] = df_sum
+# df_l = df_l.fillna(0)
 
-# 内容表記でスライスは何故かダメみたい
-data_ds = []
-for row in data_d:
-    data_ds.append(row[-days:])
+# # 内容表記でスライスは何故かダメみたい
+# data_ds = []
+# for row in data_d:
+#     data_ds.append(row[-days:])
 
-df_d = pd.DataFrame(data_ds,columns=df_date)
-df_d['name_ja'] = df_n.tail(days).copy()
-# df_d = df_d.set_index('name_ja')
-df_sum = df_d.sum(axis=1)
-df_d['sum'] = df_sum
-df_d = df_d.fillna(0)
+# df_d = pd.DataFrame(data_ds,columns=df_date)
+# df_d['name_ja'] = df_n.tail(days).copy()
+# # df_d = df_d.set_index('name_ja')
+# df_sum = df_d.sum(axis=1)
+# df_d['sum'] = df_sum
+# df_d = df_d.fillna(0)
 
 
-marker_s = [
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s", 
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s", 
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s",
-            "p", 
-            "^", 
-            "1", 
-            "8", 
-            "s"
-            ]
+# marker_s = [
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s", 
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s", 
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s",
+#             "p", 
+#             "^", 
+#             "1", 
+#             "8", 
+#             "s"
+#             ]
 
-# 都道府県別死亡者数
+# # 都道府県別死亡者数
 
-df = df_d[df_d['sum'] > 300]
-df = df.head(10)
-idx = len(df)
+# df = df_d[df_d['sum'] > 300]
+# df = df.head(10)
+# idx = len(df)
 
-df_name = df_d['name_ja']
-df_c = df.drop(['name_ja','sum'],axis=1)
-df_col = df_c.columns.values
+# df_name = df_d['name_ja']
+# df_c = df.drop(['name_ja','sum'],axis=1)
+# df_col = df_c.columns.values
 
-# days = 300
-day_m = 0 - days
-df_col = df_col[day_m:]
-df_col = pd.to_datetime(df_col).copy()
+# # days = 300
+# day_m = 0 - days
+# df_col = df_col[day_m:]
+# df_col = pd.to_datetime(df_col).copy()
 
-# df_c = df_c.astype(int).copy()
+# # df_c = df_c.astype(int).copy()
 
-# df_c.to_csv('json/test.csv')
+# # df_c.to_csv('json/test.csv')
 
-fig1, ax1 = plt.subplots(figsize=(12,8))
+# fig1, ax1 = plt.subplots(figsize=(12,8))
 
-for i in range(idx):
-    if not df_name[i] == 'ダイヤモンド・プリンセス' and not df_name[i] == '長崎のクルーズ船' and not df_name[i] == '空港検疫':
-        df_1 = df_c.iloc[i]
-        df_2 = df_1.rolling(7).mean()
-        df_2 = df_2[day_m:]
-        # ax1.plot(df_col,df_2,label=df_name[i],linewidth=2,marker=marker_s[i])
-        ax1.plot(df_col,df_2,label=df_name[i],linewidth=2)
+# for i in range(idx):
+#     if not df_name[i] == 'ダイヤモンド・プリンセス' and not df_name[i] == '長崎のクルーズ船' and not df_name[i] == '空港検疫':
+#         df_1 = df_c.iloc[i]
+#         df_2 = df_1.rolling(7).mean()
+#         df_2 = df_2[day_m:]
+#         # ax1.plot(df_col,df_2,label=df_name[i],linewidth=2,marker=marker_s[i])
+#         ax1.plot(df_col,df_2,label=df_name[i],linewidth=2)
 
-plt.grid()
-ax1.set_title('都道府県別死亡者数（移動平均） {}'.format(update))
-ax1.set_xlabel('日付')
-ax1.set_ylabel('死亡者数（人）')
-plt.legend(loc='upper left')
+# plt.grid()
+# ax1.set_title('都道府県別死亡者数（移動平均） {}'.format(update))
+# ax1.set_xlabel('日付')
+# ax1.set_ylabel('死亡者数（人）')
+# plt.legend(loc='upper left')
+
+# # """
+# # ### 都道府県別死亡者数（移動平均）
+# # """
+
+# # st.pyplot(fig1)
+
+
+# df = df_l[df_l['sum'] > 1000]
+# df = df.head(7)
+# idx = len(df)
+
+# df_name = df_l['name_ja']
+# df_l = df.drop(['name_ja','sum'],axis=1)
+# df_col = df_c.columns.values
+
+# # days = 240
+# day_m = 0 - days
+# df_col = df_col[day_m:]
+# df_col = pd.to_datetime(df_col).copy()
+
+
+# fig2, ax1 = plt.subplots(figsize=(12,8))
+
+# for i in range(idx):
+#     if not df_name[i] == 'ダイヤモンド・プリンセス' and not df_name[i] == '長崎のクルーズ船' and not df_name[i] == '空港検疫':
+#         df_1 = df_l.iloc[i]
+#         df_2 = df_1.rolling(7).mean()
+#         df_2 = df_2[day_m:]
+#         # ax1.plot(df_col,df_2,label=df_name[i],linewidth=2,marker=marker_s[i])
+#         ax1.plot(df_col,df_2,label=df_name[i],linewidth=2)
+
+
+# # mem_list = [a for a in range(0,day,14)]
+# # # plt.xticks([0,7,15,22,29])
+# # plt.xticks(mem_list)
+
+# plt.grid()
+# ax1.set_title('都道府県別感染者数（移動平均） {}'.format(update))
+# ax1.set_xlabel('日付')
+# ax1.set_ylabel('感染者数（人）')
+# plt.legend(loc='upper left')
+
+# # """
+# # ### 都道府県別感染者者数（移動平均）
+# # """
+
+# # st.pyplot(fig2)
+
+# # """
+# # #### plotly
+# # """
+
+# # st.write(
+# #     px.bar(df_a.tail(240),x='日付',y="感染者数",title='感染者数')
+# # )
+# # st.write(
+# #     px.bar(df_a.tail(240),x='日付',y="検査数",title='検査数')
+# # )
+
+
+# # st.write(
+# #     px.line(df_a.tail(days),x='日付',y='重症者累計',title='重症者累計')
+# # )
+
+# """
+# ### 都道府県別感染者数（移動平均）
+# """
+
+# st.pyplot(fig2)
 
 # """
 # ### 都道府県別死亡者数（移動平均）
@@ -308,75 +379,82 @@ plt.legend(loc='upper left')
 
 # st.pyplot(fig1)
 
+# ### 都道府県別感染者数（移動平均）
 
-df = df_l[df_l['sum'] > 1000]
-df = df.head(7)
-idx = len(df)
+datalist = [
+            [row['date']
+             ] 
+            for row in summary_json['daily']
+            ]
 
-df_name = df_l['name_ja']
-df_l = df.drop(['name_ja','sum'],axis=1)
-df_col = df_c.columns.values
+df_h = pd.DataFrame(datalist,columns=['日付'])
+df_h = pd.to_datetime(df_h['日付'])
+df_h = df_h[-300:]
 
-# days = 240
-day_m = 0 - days
-df_col = df_col[day_m:]
-df_col = pd.to_datetime(df_col).copy()
+data_n = [str(i+1).zfill(2)+':'+row['name_ja'] for i,row in enumerate(summary_json['prefectures'])] #都道府県名
+data_l = [row['dailyConfirmedCount'] for row in summary_json['prefectures']] #感染者数
+data_d = [row['dailyDeceasedCount'] for row in summary_json['prefectures']] #死亡者数
 
+data_ls = []
+for row in data_l:
+    data_ls.append(row[-300:])
 
-fig2, ax1 = plt.subplots(figsize=(12,8))
+df_l = pd.DataFrame(data_ls,columns=df_h)
+df_s = []
+for row in data_l:
+    df_s.append(sum(row))
 
-for i in range(idx):
-    if not df_name[i] == 'ダイヤモンド・プリンセス' and not df_name[i] == '長崎のクルーズ船' and not df_name[i] == '空港検疫':
-        df_1 = df_l.iloc[i]
-        df_2 = df_1.rolling(7).mean()
-        df_2 = df_2[day_m:]
-        # ax1.plot(df_col,df_2,label=df_name[i],linewidth=2,marker=marker_s[i])
-        ax1.plot(df_col,df_2,label=df_name[i],linewidth=2)
+df_l.insert(0,'都道府県',pd.DataFrame(data_n))
+df_l.insert(1,'合計',pd.DataFrame(df_s))
 
+erea_list = list(df_l['都道府県'].unique())
+selected_erea = st.multiselect('グラフに表示する都道府県を選択', erea_list, default=erea_list[:7])
+df = df_l[(df_l['都道府県'].isin(selected_erea))]
 
-# mem_list = [a for a in range(0,day,14)]
-# # plt.xticks([0,7,15,22,29])
-# plt.xticks(mem_list)
+df_1 = df.T
+df_1 = df_1.drop(['都道府県','合計'],axis=0)
+df_1 = df_1.rolling(7).mean()
+dfl = df_1.values.tolist()
+selected_erea = sorted(selected_erea)
+df_xx = pd.DataFrame(dfl,columns=selected_erea)
+df_xx = df_xx.set_index(df_h)
 
-plt.grid()
-ax1.set_title('都道府県別感染者数（移動平均） {}'.format(update))
-ax1.set_xlabel('日付')
-ax1.set_ylabel('感染者数（人）')
-plt.legend(loc='upper left')
+st.title('都道府県別感染者数(移動平均)')
+if len(df) > 0:
+    st.line_chart(df_xx,use_container_width=True)
+else:
+    st.write('対象の都道府県を選択してください')
 
-# """
-# ### 都道府県別感染者者数（移動平均）
-# """
+# ### 都道府県別死亡者数（移動平均）
+data_ds = []
+for row in data_d:
+    data_ds.append(row[-300:])
 
-# st.pyplot(fig2)
+df_d = pd.DataFrame(data_ds,columns=df_h)
+df_s = []
+for row in data_d:
+    df_s.append(sum(row))
 
-# """
-# #### plotly
-# """
+df_d.insert(0,'都道府県',pd.DataFrame(data_n))
+df_d.insert(1,'合計',pd.DataFrame(df_s))
 
-# st.write(
-#     px.bar(df_a.tail(240),x='日付',y="感染者数",title='感染者数')
-# )
-# st.write(
-#     px.bar(df_a.tail(240),x='日付',y="検査数",title='検査数')
-# )
+# erea_list = list(df_d['都道府県'].unique())
+# selected_erea = st.multiselect('グラフに表示する都道府県を選択', erea_list, default=erea_list[:7])
+df = df_d[(df_d['都道府県'].isin(selected_erea))]
 
+df_1 = df.T
+df_1 = df_1.drop(['都道府県','合計'],axis=0)
+df_1 = df_1.rolling(7).mean()
+dfl = df_1.values.tolist()
+selected_erea = sorted(selected_erea)
+df_xx = pd.DataFrame(dfl,columns=selected_erea)
+df_xx = df_xx.set_index(df_h)
 
-# st.write(
-#     px.line(df_a.tail(days),x='日付',y='重症者累計',title='重症者累計')
-# )
-
-"""
-### 都道府県別感染者数（移動平均）
-"""
-
-st.pyplot(fig2)
-
-"""
-### 都道府県別死亡者数（移動平均）
-"""
-
-st.pyplot(fig1)
+st.title('都道府県別死亡者数(移動平均)')
+if len(df) > 0:
+    st.line_chart(df_xx,use_container_width=True)
+else:
+    st.write('対象の都道府県を選択してください')
 
 # ここから東京都
 
